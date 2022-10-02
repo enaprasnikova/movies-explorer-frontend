8429ca33b1a877bcdb01b75a4f35959d61988051
Elena Naprasnikova <ev.naprasnikova@gmail.com>
2022-10-02 23:06:07 +0300

    Функциональность на «React» и JS (проверка Л1)

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3001';
+const BASE_URL = `${window.location.protocol}${process.env.REACT_APP_API_URL || 'http://localhost:3001'}`;
 
 export const register = (password, email, name) => {
   return fetch(`${BASE_URL}/signup`, {
